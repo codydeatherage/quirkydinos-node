692952127e4fbdf1466e51d59cffc9d9e127c706
Cody Deatherage <cdeatherage@koiconsults.com>
2024-08-07 14:56:48 -0700

    testing mysql connection

--- index.js
+++ index.js
@@ -9,12 +9,28 @@ const corsOptions = {
   allowedHeaders: ["Content-Type"],
 };
 
+const db = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+app.use(express.json());
 app.use(cors(corsOptions));
 
 const PORT = process.env.port ?? 3000;
 
 app.get("/", (req, res) => {
   res.json("Hello QuirkyDinos");
+});
+
+app.get("/test", (req, res) => {
+  const q = `SELECT * FROM test`;
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
 });
 
 app.listen(PORT, () => {
